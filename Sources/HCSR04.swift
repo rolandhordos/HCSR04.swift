8dd6df66b076b88bc181b4478709f10342b5a52d
Roland Hordos <rolandhordos@mac.com>
2022-01-06 17:12:30 -0800

    * return to original author overall error style * use DispatchTimeInterval for durations given that DispatchTime is central * some rework avoiding diff calculations while spinning * speed of sound can be set

--- Sources/HCSR04.swift
+++ Sources/HCSR04.swift
@@ -2,13 +2,6 @@ import SwiftyGPIO
 import Foundation
 import Dispatch
 
-public enum HCSR04Error: Error {
-    case echoSignalError                                        // check pin connections, timeout, and range
-    case measuredDistanceIsOutSensorRange
-    case userTimeout
-    case unavailableGPIO(detail: String)
-}
-
 /// Create an instance representing the HC-SR04 hardware module
 ///
 /// Avoiding *pin* terminology favoring GPIO## as that's what the SwiftyGPIO `.P##` enumeration cases are (vs board pin #).
@@ -17,11 +10,21 @@ public enum HCSR04Error: Error {
 /// Default maximum distance comes from [the datasheet](https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf)
 ///
 open class HCSR04 {
+
+    public enum Error: Swift.Error {
+        case echoSignalError(String? = nil)
+        case measuredDistanceIsOutSensorRange
+        case userTimeout
+        case unavailableGPIO(String? = nil)
+    }
+
     public typealias CentiMeters = Double
+    public typealias MetersPerSecond = Double
     
     public var triggerGPIO    : GPIO?                           // ultrasonic emitter sends first
     public var echoGPIO       : GPIO?                           // ultrasonic receiver then receives returned sound waves
-    public var maximumDistance: CentiMeters = 400
+    public var maximumDistance: CentiMeters     = 400           // default upper range from datasheet
+    public var speedOfSound   : MetersPerSecond = 343           // from wikipedia as speed of sound in air, note datasheet says 340
 
     /// No-arg constructor, make sure to assign `triggerGPIO` and `echoGPIO` or use optional convenience constructor
     ///
@@ -49,12 +52,12 @@ open class HCSR04 {
         let gpios = SwiftyGPIO.GPIOs(for: board)
 
         guard let echoGPIO = gpios[echo] else {
-            throw HCSR04Error.unavailableGPIO(detail: "Echo GPIO number \(echo) not available, check power and wiring")
+            throw Error.unavailableGPIO("Echo GPIO number \(echo) not available, check power and wiring")
         }
         echoGPIO.direction = .IN
         
         guard let triggerGPIO = gpios[trigger] else {
-            throw HCSR04Error.unavailableGPIO(detail: "Trigger GPIO number \(echo) not available, check power and wiring")
+            throw Error.unavailableGPIO("Trigger GPIO number \(echo) not available, check power and wiring")
         }
         triggerGPIO.direction = .OUT
 
@@ -62,77 +65,80 @@ open class HCSR04 {
         self.echoGPIO = echoGPIO
     }
     
-    open func measureDistance(numberOfSamples: Int? = nil, providedTimeout: Int? = nil) throws -> Double {
+    
+    /// Return measured average distance
+    ///
+    /// Timeout will be calculated by default based on maximum distance.  Caution if setting a large timeout as this function is very expensive,
+    /// spinning in a while loop during pulse detection.  Setting a timeout includes the time to take *all* samples
+    ///
+    /// Times are in nanoseconds unless specified, note that TimeInterval is in seconds
+    ///
+    open func measureDistance(numberOfSamples: Int = 1, timeout: DispatchTimeInterval? = nil) throws -> CentiMeters {
         guard let echoGPIO = echoGPIO else {
-            throw HCSR04Error.unavailableGPIO(detail: "Unable to read distance input due to missing echo input GPIO")
+            throw Error.unavailableGPIO("Unable to read distance input due to missing echo input GPIO")
         }
 
-        var beginningTimeOfEchoSignal: DispatchTime //The beginning of echo signal.
-        var endTimeOfEchoSignal: DispatchTime //The end of echo signal.
-        var echoSignalTime = Double.init() //Calculated echo signal time.
-        var distance = Double.init() //Calculated distance.
-        var enterTimeIntoWhile: DispatchTime //Used for timeout and error detection.
-        let maximumEchoSignalTime = (maximumDistance/0.0000343) * 2 //Time of maximum echo signal for provided sensor range - used for error detection and default timeout.
-        let defaultTimeout = maximumEchoSignalTime * 2  //Calculate timeout = (maximumEchoSignalTime)*(safety margin).
-        let usedTimeout: Double //Finally used timeout - default or provided by user.
-        
-        if (providedTimeout == nil) {
-            usedTimeout = defaultTimeout
-        } else {
-            usedTimeout = Double(providedTimeout! * 1000000) //convert miliseconds to nanoseconds, user provide timeout in miliseconds.
-        }
-        
-        for _ in 0..<(numberOfSamples ?? 1) { //Default number of samples is 1, user can provide another number of samples by optional argument while calling method measureDistance
+        let maximumTravel: CentiMeters = maximumDistance * 2                            // there and back
+        let maximumTravelTime: TimeInterval = maximumTravel / (speedOfSound * 100)      // 100 cm in a meter
+        let maximumTravelTimeAllSamples: TimeInterval = maximumTravelTime * Double(numberOfSamples)
+
+        // careful converting so we don't truncate the double
+        let maximumEchoSignalDuration: DispatchTimeInterval = .milliseconds(Int(maximumTravelTime * 1000.0))
+
+        // use provided timeout or use theoretical maximum padded with a feels-good margin
+        let paddingFactor = 1.5
+        let timeoutDuration: DispatchTimeInterval = timeout ?? .milliseconds(Int(maximumTravelTimeAllSamples * 1000.0 * paddingFactor))
+
+        let timeoutAt = DispatchTime.now() + timeoutDuration                            // timeout as a fixed point in time
+        var totalDistance: CentiMeters = 0                                              // tally over all samples
+        for sample in 1..<numberOfSamples + 1 {
             
             //Start distance measure.
             try generateTriggerImpulse() //Generate trigger impuls 10 microseconds long.
             
-            enterTimeIntoWhile = DispatchTime.now() //Save enter time into while loop for error detection.
-            while (echoGPIO.value == 0) {
-                if (calculateTimeInterval(from: enterTimeIntoWhile, to: DispatchTime.now()) > usedTimeout){
-                    throw HCSR04Error.echoSignalError //Throw error
+            // Pulse detection starting with low signal on leading edge
+            while echoGPIO.value == 0 {
+                if DispatchTime.now() > timeoutAt {
+                    throw Error.echoSignalError("Timeout occurred during front edge of pulse detection")
                 }
             }
-            beginningTimeOfEchoSignal = DispatchTime.now() //Save time of  beginning echo signal.
-            
-            enterTimeIntoWhile = DispatchTime.now() //Save enter time into while loop for error detection.
-            while (echoGPIO.value == 1){ //Wait for end of echo signal.
-                let timeInLoop = calculateTimeInterval(from: enterTimeIntoWhile, to: DispatchTime.now())
-                if timeInLoop >= maximumEchoSignalTime {
-                    throw HCSR04Error.measuredDistanceIsOutSensorRange //Throw error.
-                } else if (providedTimeout != nil) && (timeInLoop > usedTimeout) {
-                    throw HCSR04Error.userTimeout //Throw error - user timeout interrupt.
+
+            let beginningTimeOfEchoSignal = DispatchTime.now() //Save time of  beginning echo signal.
+
+            // Detect the rise in pulse and measure it's duration
+            let maximumEchoAt = DispatchTime.now() + maximumEchoSignalDuration
+            while echoGPIO.value == 1 { //Wait for end of echo signal.
+                let now = DispatchTime.now()
+                if now > maximumEchoAt {
+                    throw Error.measuredDistanceIsOutSensorRange //Throw error.
+                } else if (timeout != nil) && (now > timeoutAt) {
+                    throw Error.userTimeout //Throw error - user timeout interrupt.
                 }
             }
-            endTimeOfEchoSignal = DispatchTime.now() //Save time of the end echo signal.
-            
-            echoSignalTime = calculateTimeInterval(from: beginningTimeOfEchoSignal, to: endTimeOfEchoSignal) //Calculate time of echo signal.
-            distance = distance + (echoSignalTime * 0.0000343)/2 //Calculate distance: ((echo signal time in nanosecodns)*(speed of the sound cm per nanoseconds))/(distance divided by 2, echo signal round trip)).
-            if numberOfSamples != nil {
+            let endTimeOfEchoSignal = DispatchTime.now()
+
+            let echoSignalTime = endTimeOfEchoSignal.uptimeNanoseconds - beginningTimeOfEchoSignal.uptimeNanoseconds
+            totalDistance = totalDistance + (Double(echoSignalTime) * 0.0000343)/2 //Calculate distance: ((echo signal time in nanosecodns)*(speed of the sound cm per nanoseconds))/(distance divided by 2, echo signal round trip)).
+            if sample < numberOfSamples {
                 usleep(60000) //Wait 60ms before next sample measurement.
             }
         }
-        distance = distance/Double(numberOfSamples ?? 1) //Calculate average distance.
         
-        if distance <= maximumDistance {
-            return distance
-        } else {
-            throw HCSR04Error.measuredDistanceIsOutSensorRange
+        let averageDistance = totalDistance / Double(numberOfSamples)
+        guard averageDistance <= maximumDistance else {
+            throw Error.measuredDistanceIsOutSensorRange
         }
+        return averageDistance
     }
     
     open func generateTriggerImpulse() throws {
         guard let triggerGPIO = triggerGPIO else {
-            throw HCSR04Error.unavailableGPIO(detail: "Unable to trigger sonic impulse due to missing trigger output GPIO")
+            throw Error.unavailableGPIO("Unable to trigger sonic impulse due to missing trigger output GPIO")
         }
 
         triggerGPIO.value = 1 //Set trigger pin High level.
         usleep(10)//Wait 10 microseconds.
         triggerGPIO.value = 0 //Set trigger pin Low level.
-    }
-    
-    private func calculateTimeInterval(from startTime: DispatchTime, to endTime: DispatchTime) -> Double {
-        return Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds)
     }
 }
 
